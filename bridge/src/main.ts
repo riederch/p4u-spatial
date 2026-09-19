c30c4ed4381667e46b577b1ed55267fb3347dbfc
Christoph <rieder.christoph@gmx.at>
2026-09-19 20:51:54 +0200

    feat: load bridge configuration from persistent state

--- bridge/src/main.ts
+++ bridge/src/main.ts
@@ -1,9 +1,12 @@
 import { buildServer } from "./server.js";
 import { loadConfig } from "./config.js";
 import { createRepositoryProvider } from "./repository/factory.js";
+import { BridgeConfigStore } from "./services/bridge-config-store.js";
 
-const config = loadConfig();
+const bootstrapConfig = loadConfig();
+const configStore = new BridgeConfigStore(bootstrapConfig.stateDir, bootstrapConfig);
+const config = await configStore.loadOrMigrate();
 const repository = await createRepositoryProvider(config);
-const app = buildServer(config, repository);
+const app = buildServer(config, repository, { configStore });
 
 await app.listen({ host: config.host, port: config.port });
