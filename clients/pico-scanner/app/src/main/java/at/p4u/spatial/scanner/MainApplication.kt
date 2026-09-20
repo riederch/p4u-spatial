97768e05c4619197151a9832a446fb433dae5ec9
Christoph <rieder.christoph@gmx.at>
2026-09-20 15:07:05 +0200

    fix(picovr): register PICO spatial window container

---
+++ clients/pico-scanner/app/src/main/java/at/p4u/spatial/scanner/MainApplication.kt
@@ -0,0 +1,20 @@
+package at.p4u.spatial.scanner
+
+import android.app.Application
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.pico.spatial.ui.foundation.dsl.DefaultWindowContainer
+import com.pico.spatial.ui.foundation.dsl.launch
+import com.pico.spatial.ui.foundation.windowConstraints
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        launch {
+            DefaultWindowContainer {
+                Box(Modifier.windowConstraints(width = 960.dp, height = 720.dp))
+            }
+        }
+    }
+}
