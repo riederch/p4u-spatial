d06a939b961e8e850e9442c416868b9a5b32ae08
Christoph <rieder.christoph@gmx.at>
2026-09-21 13:09:45 +0200

    build(picovr): add platform API for view-following HUD

--- clients/pico-scanner/app-ui/build.gradle.kts
+++ clients/pico-scanner/app-ui/build.gradle.kts
@@ -30,5 +30,6 @@ dependencies {
     implementation(project(":app-core"))
     implementation(platform("com.pico.spatial:bom:6.1.9"))
     implementation("com.pico.spatial.ui:foundation")
+    implementation("com.pico.spatial.ui:platform")
     implementation("com.pico.spatial.ui:design")
 }
