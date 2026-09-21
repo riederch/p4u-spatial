48a52bd63385f3757a37df0c991039beee9c13c2
Christoph <rieder.christoph@gmx.at>
2026-09-21 13:00:41 +0200

    feat(picovr): move QR presentation into feature UI module

---
+++ clients/pico-scanner/qr-reader-ui/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "at.p4u.picovr.qr.ui"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 31
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(project(":app-core"))
+    implementation(project(":app-ui"))
+    implementation(project(":qr-reader"))
+    implementation(platform("com.pico.spatial:bom:6.1.9"))
+    implementation("com.pico.spatial.ui:design")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+}
